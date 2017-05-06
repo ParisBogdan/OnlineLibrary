fca03bc99dc573b81d7e325924bf711f15ae9780
marinela.ginga <marinela.ginga@PC140>
2017-05-06 13:46:11 +0300

    Add project files.

---
+++ OnlineLibrary.Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using OnlineLibrary.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Services
+{
+  public interface IUserService
+  {
+    List<User> GetAllUsers();
+        void CreateUser(User newUser);
+      List<User> GetList(int pageNumber, int pageSize, string sort, string sortDir);
+      int Count();
+  }
+}
---
+++ OnlineLibrary.Services/UserService.cs
@@ -0,0 +1,43 @@
+﻿using OnlineLibrary.Data.Infrastructure;
+using OnlineLibrary.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Services
+{
+    public class UserService : IUserService
+    {
+    private IRepository<User> userRepo;
+    private IUnitOfWork unitOfWork;
+
+    public UserService(IRepository<User> userRepo, IUnitOfWork unitOfWork)
+    {
+      this.userRepo = userRepo;
+      this.unitOfWork = unitOfWork;
+    }
+
+    public List<User> GetAllUsers()
+    {
+     return userRepo.GetAll().ToList();    
+    }
+        public void CreateUser(User user)
+        {
+            userRepo.Add(user);
+            unitOfWork.Commit();
+        }
+        public List<User> GetList(int pageNumber, int pageSize, string sort, string sortDir)
+        {   
+            //2
+            return userRepo.GetAll().Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            //return userRepo.GetAll((pageNumber - 1) * pageSize, pageSize, sort, sortDir).ToList();
+        }
+
+        public int Count()
+        {
+            return userRepo.Count();
+        }
+    }
+}
